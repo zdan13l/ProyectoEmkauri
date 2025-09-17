dbe2ac09c80e40a18d1424cc9023bd977d9feeb2
zdan1 <ortiz.jd@javeriana.edu.co>
2025-09-16 20:14:34 -0500

    Estructura básica del proyecto separando en carpetas cada función

---
+++ src/main/java/servicio/SCompra.java
@@ -0,0 +1,14 @@
+package servicio;
+
+import modelo.Compra;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SCompra {
+    private List<Compra> compras;
+
+    public SCompra() {
+        this.compras = new ArrayList<>();
+    }
+}
---
+++ src/main/java/servicio/SUsuario.java
@@ -0,0 +1,14 @@
+package servicio;
+
+import modelo.Usuario;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SUsuario {
+    private List<Usuario> usuarios;
+
+    public SUsuario() {
+        this.usuarios = new ArrayList<>();
+    }
+}
